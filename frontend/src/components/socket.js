971ab889baa1e0bda2cf9f4821f942b73ec1fb91
Hector Acosta <1Hector.AC@gmail.com>
2026-06-03 11:30:46 -0700

    Edit layout in Create.jsx. Added noVideo image. Small url reference related changes in socket.io & gitignore.

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client'
 
-const URL = "";
+const URL = import.meta.env.VITE_API_URL;
 
 export default io(URL);
+
+//{autoConnect: false, transports: ["websocket"]}
--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -1,25 +1,63 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import videoPlaceholder from '../assets/noVideo.jpg'
 
 function Create() {
     const [room, setRoom] = useState();
     const [userName, setUserName] = useState();
     const [error, setError] = useState();
+    const [onVideoState, setOnVideoState] = useState(false);
+    const [localIsPaused, setLocalIsPaused] = useState(true);
+    const [localIsMute, setLocalIsMute] = useState(true);
+    const [remoteIsMute, setRemoteIsMute] = useState(true);
+
+    const localVideo = useRef();
+    const remoteVideo = useRef();
+
     function onClickCreate() {
         if (!room || !userName) {
             setError("All fields must be entered");
+            return;
         }
+        setError("");
+        setOnVideoState(true);
     }
     return (
         <div>
-            <h1>Create Session</h1>
-            <label htmlFor="userName">userName</label>
-            <input name="userName" id="userName" onChange={e => setRoom(e.target.value)} />
-            <br />
-            <label htmlFor="room">room</label>
-            <input name="room" id="room" onChange={e => setUserName(e.target.value)} />
-            <br />
-            <button onClick={onClickCreate}>Create</button>
-            {error && <p className="errorMessage">{error}</p>}
+            <h1>Host video Session</h1>
+            {!onVideoState ?
+                <div>
+                    <label htmlFor="userName">userName</label>
+                    <input name="userName" id="userName" onChange={e => setRoom(e.target.value)} />
+                    <br />
+                    <label htmlFor="room">room</label>
+                    <input name="room" id="room" onChange={e => setUserName(e.target.value)} />
+                    <br />
+                    <button onClick={onClickCreate}>Create</button>
+                    {error && <p className="errorMessage">{error}</p>}
+                </div>
+                :
+                <div style={{ display: 'flex', gap: 20 }}>
+                    <div>
+                        <p>me</p>
+                        <video ref={localVideo} poster={videoPlaceholder} autoPlay playsInline muted width={300}></video>
+                    </div>
+                    <div>
+                        <p>Other</p>
+                        <video ref={remoteVideo} poster={videoPlaceholder} autoPlay playsInline width={400}></video>
+
+                    </div>
+                    <div>
+                        <button>{localIsPaused ? "Unpause Video" : "Pause Video"}</button>
+                        <button>{localIsMute ? "Unmute" : "Mute"}</button>
+                        <button>{remoteIsMute ? "Unmute Other" : "Mute Other"}</button>
+                        <button>Disconnect</button>
+
+                    </div>
+
+                </div>
+            }
+
+
         </div>
     )
 }
